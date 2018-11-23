b8d6219040014a0e9327e1ac1cba98e3185370fb
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2018-11-23 15:29:32 +0100

    assembler: fix Instruction::encode

--- assembler/src/instruction.rs
+++ assembler/src/instruction.rs
@@ -13,9 +13,9 @@ impl Instruction {
     pub fn encode(&self) -> u64 {
         let mut res: u64 = 0;
         res |= self.opcode as u64;
-        res |= (self.rs1 as u64) << 7;
-        res |= (self.rs2 as u64) << 11;
-        res |= (self.rd as u64) << 15;
+        res |= (self.rd as u64) << 7;
+        res |= (self.rs1 as u64) << 11;
+        res |= (self.rs2 as u64) << 15;
         res |= (self.immediate as u64) << 19;
         res
     }

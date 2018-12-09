029553f519d1d83ab6968f29cd64d0480e01eae7
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2018-12-09 20:35:04 +0100

    assembler: small code cleanup for c52ec353fe69b0914edd68d2a1cda4fc3424dca2

--- assembler/src/util.rs
+++ assembler/src/util.rs
@@ -2,22 +2,19 @@ use std::num;
 use std::result::Result;
 
 pub fn parse_number(s: &str) -> Result<i64, num::ParseIntError> {
-    let s2;
-    let sign;
-    if s.starts_with("-") {
-        s2 = &s[1..];
-        sign = -1;
+    let (s, sign) = if s.starts_with("-") {
+        (&s[1..], -1)
     } else {
-        s2 = &s[..];
-        sign = 1;
-    }
-
-    let r = if s2.starts_with("0x") {
-        i64::from_str_radix(&s2[2..], 16)
-    } else if s2.starts_with("0b") {
-        i64::from_str_radix(&s2[2..], 8)
-    } else {
-        i64::from_str_radix(s2, 10)
+        (s, 1)
     };
-    r.map(|n| n * sign)
+
+    let n = if s.starts_with("0x") {
+        i64::from_str_radix(&s[2..], 16)
+    } else if s.starts_with("0b") {
+        i64::from_str_radix(&s[2..], 8)
+    } else {
+        i64::from_str_radix(s, 10)
+    };
+
+    n.map(|n| n * sign)
 }
